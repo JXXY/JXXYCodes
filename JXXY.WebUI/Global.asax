5f974c862a6c8e1f9bec32edc75a61ddf2e4b35b
chinayinhui <yhhjf78910>
2016-02-06 10:25:17 +0800

    Created solution

---
+++ JXXY.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JXXY.WebUI.MvcApplication" Language="C#" %>
---
+++ JXXY.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JXXY.WebUI.Startup))]
+namespace JXXY.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
