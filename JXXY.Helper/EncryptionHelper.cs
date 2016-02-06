2f75425e248e015bebb700ef7fa8ea3b5b765411
chinayinhui <yhhjf78910>
2016-02-06 11:25:34 +0800

    Added encription class

---
+++ JXXY.Helper/EncryptionHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JXXY.Helper
+{
+    public static class EncryptionHelper
+    {
+        public static string EncryptPassword(string password)
+        {
+            string salt = Guid.NewGuid().ToString();            
+
+            byte[] passwordAndSaltBytes = System.Text.Encoding.UTF8.GetBytes(password + salt);
+            byte[] hashBytes = new System.Security.Cryptography.SHA256Managed().ComputeHash(passwordAndSaltBytes);
+            string hashString = Convert.ToBase64String(hashBytes);
+            return hashString;
+        } 
+    }
+}
