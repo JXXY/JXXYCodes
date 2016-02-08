a091ca1e6a21fd5a6bbbfff92e4eabaf9e9085ef
chinayinhui <yhhjf78910>
2016-02-08 11:24:42 +0800

    Added menus.

---
+++ JXXY.WebUI/Controllers/SchoolController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JXXY.WebUI.Controllers
+{
+    public class SchoolController : Controller
+    {
+        // GET: School
+        public ActionResult Show()
+        {
+            return View();
+        }
+    }
+}
